abe954060dd057d64e4b5bc19b0eb376dc90a7d6
Iamneo <techadmin@iamneo.ai>
2023-09-03 17:45:22 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -27,6 +27,21 @@ function checkWin(){
         [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
     ];
     return winningCombination.some(combination=>{
-        const
-    })
+        const[a,b,c]=combination;
+        return board[a]&&board[a]===board[b]&&board[a]===board[c];
+
+    });
 }
+function resetBoard(){
+    currentPlayer='X';
+    board=['','','','','','','','',''];
+    gameActive=true;
+    document.querySelectorAll('.cell').forEach(cell=>{
+        cell.textContent='';
+        cell.classList.remove('X','0');
+
+    });
+    document.querySelector('.result p').textContent="Player X's turn";
+
+}
+resetBoard();
