d2ca317894d6425c4e4b818a53c96bb2cfaf3894
Iamneo <techadmin@iamneo.ai>
2023-09-03 17:35:22 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let currentPlayer='X'
+let currentPlayer='X';
+let board=['','','','','','','','',''];
+let gameActive=true;
+function makeMove(cell){
+    const cellIndex=Array.from(cell.parentNode.children),indexOf(cell);
+    if(board[cellIndex]===''&& gameActive){
+        board[cellIndex]=currentPlayer;
+        cell.textContent=currentPlayer;
+        cell.classList.add(currentPlayer);
+        if(checkWin()){
+            document.querySelector(',result p').textContent='player ${currentPlayer} wins!';
+            gameActive=false;
+
+        }else if (!board.included)
+    }
+}
